89fd60efce111a07fa1f47e9f44be1aafcce1633
unknown <koech9524@gmail.com>
2024-02-20 22:35:00 +0300

    power of two

---
+++ Power_of_Two.py
@@ -0,0 +1,8 @@
+def is_power_of_two(n):
+    if n <= 0:
+        return False
+    return (n & (n - 1)) == 0
+
+# Test the function
+num = int(input("Enter an integer: "))
+print(is_power_of_two(num))
